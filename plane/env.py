83df5683d4bf23c4ee54b1a22d4840a62baf4785
Yann Berthelot <yannberthelot1@gmail.com>
2023-11-15 16:46:46 +0100

    refactoring

--- plane/env.py
+++ plane/env.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+import jax
 import chex
 import jax.numpy as jnp
 from flax import struct
@@ -7,8 +7,16 @@ from gymnax.environments import environment, spaces
 from jax import lax
 
 from plane.utils import compute_norm_from_coordinates
+from plane.dynamics import (
+    compute_acceleration,
+    compute_next_power,
+    compute_thrust_output,
+    compute_speed_and_pos_from_acceleration,
+    compute_air_density_from_altitude,
+)
 
 Action = Any
+SPEED_OF_SOUND = 343
 
 
 @struct.dataclass
@@ -44,7 +52,7 @@ class EnvState:
         """
         The mach number
         """
-        return compute_norm_from_coordinates([self.x_dot, self.z_dot]) / 340
+        return compute_norm_from_coordinates([self.x_dot, self.z_dot]) / SPEED_OF_SOUND
 
     # simulation
     t: int  # the time (in seconds TODO : check this) since starting the simulation.
@@ -72,18 +80,86 @@ class EnvParams:
         17.5 / 1000
     )  # thrust-specific fuel consumption (kg.kN^-1.s-1)
     delta_t: int = 1  # timestep size (in seconds)
-    speed_of_sound: float = 340  # speed of sounds (in m.s-1)
+    speed_of_sound: float = SPEED_OF_SOUND  # speed of sounds (in m.s-1)
 
 
-def compute_next_state(action: Action, state: EnvState, params: EnvParams) -> EnvState:
-    raise NotImplementedError
+def check_mass_does_not_increase(old_mass, new_mass):
+    assert old_mass >= new_mass
+
+
+def compute_next_state(
+    power_requested: float, state: EnvState, params: EnvParams
+) -> EnvState:
+    # power
+    power = compute_next_power(power_requested, state.power)
+    thrust = compute_thrust_output(
+        power=power,
+        altitude_factor=state.atltitude_factor,
+        thrust_output_at_sea_level=params.thrust_output_at_sea_level,
+    )
+    # acceleration, speed and position
+    a_x, a_z = compute_acceleration(
+        thrust=thrust,
+        m=state.m,
+        gravity=params.gravity,
+        x_dot=state.x_dot,
+        z_dot=state.z_dot,
+        air_density_at_sea_level=params.air_density_at_sea_level,
+        atltitude_factor=state.atltitude_factor,
+        frontal_surface=params.frontal_surface,
+        wings_surface=params.wings_surface,
+        alpha=state.alpha,
+        M=state.M,
+        M_crit=params.M_crit,
+        C_x0=params.C_x0,
+        C_z0=params.C_z0,
+        gamma=state.gamma,
+        theta=state.theta,
+    )
+    (
+        x_dot,
+        z_dot,
+        x,
+        z,
+    ) = compute_speed_and_pos_from_acceleration(
+        state.x_dot, state.z_dot, state.x, state.z, a_x, a_z, params.delta_t
+    )
+
+    # time
+    t = state.t + 1
+
+    # angles
+    gamma = jnp.arcsin(z_dot / compute_norm_from_coordinates([x_dot, z_dot + 1e-6]))
+    alpha = state.theta - gamma
+
+    # mass
+    m = params.initial_mass + state.fuel
+    jax.debug.callback(check_mass_does_not_increase, state.m, m)
+
+    new_state = EnvState(
+        x=x,
+        x_dot=x_dot,
+        z=z,
+        z_dot=z_dot,
+        theta=state.theta,  # no change atm
+        alpha=alpha,
+        gamma=gamma,
+        m=m,  # no change atm
+        power=power,
+        fuel=state.fuel,  # no change atm
+        rho=compute_air_density_from_altitude(
+            params.air_density_at_sea_level, altitude_factor=state.atltitude_factor
+        ),
+        t=t,
+    )
+    return new_state
 
 
 def compute_reward(state: EnvState, params: EnvParams) -> float:
     raise NotImplementedError
 
 
-class CartPole(environment.Environment):
+class Airplane2D(environment.Environment):
     """
     JAX Compatible version of CartPole-v1 OpenAI gym environment. Source:
     github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
@@ -95,7 +171,7 @@ class CartPole(environment.Environment):
 
     @property
     def default_params(self) -> EnvParams:
-        # Default environment parameters for CartPole-v1
+        # Default environment parameters for Airplane2D
         return EnvParams()
 
     def step_env(
@@ -107,7 +183,6 @@ class CartPole(environment.Environment):
         state = compute_next_state(action, state, params)
         reward = compute_reward(state, params)
         # Update state dict and evaluate termination conditions
-        state = EnvState()  # Todo : fill
         done = self.is_terminal(state, params)
 
         return (
