f0c301b2e3bef7c86baac326a909110f820efff7
Yann Berthelot <yannberthelot1@gmail.com>
2023-11-18 09:46:39 +0100

    debugged surfaces + added plot utils

--- tests/test_physics.py
+++ tests/test_physics.py
@@ -3,17 +3,13 @@ from math import cos, sin
 import numpy as np
 import pytest
 
-from plane.dynamics import (
-    compute_air_density_from_altitude,
-    compute_drag,
-    compute_exposed_surfaces,
-    compute_initial_x_drag_coefficient,
-    compute_initial_z_drag_coefficient,
-    compute_mach_impact_on_x_drag_coefficient,
-    compute_mach_impact_on_z_drag_coefficient,
-    compute_weight,
-    newton_second_law,
-)
+from plane.dynamics import (compute_air_density_from_altitude, compute_drag,
+                            compute_exposed_surfaces,
+                            compute_initial_x_drag_coefficient,
+                            compute_initial_z_drag_coefficient,
+                            compute_mach_impact_on_x_drag_coefficient,
+                            compute_mach_impact_on_z_drag_coefficient,
+                            compute_weight, newton_second_law)
 from plane.env import EnvParams, EnvState, compute_next_state
 
 
