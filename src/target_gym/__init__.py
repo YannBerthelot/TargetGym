e09723eb04901241754fc080a2e5ab2fe921ee7c
YannBerthelot <yannberthelot1@gmail.com>
2025-09-21 09:51:36 +0200

    automate version setting

--- src/target_gym/__init__.py
+++ src/target_gym/__init__.py
@@ -1,3 +1,5 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from target_gym.bicycle.env import EnvParams as BikeParams
 from target_gym.bicycle.env_jax import RandlovBicycle as Bike
 from target_gym.car.env import EnvParams as CarParams
@@ -5,6 +7,11 @@ from target_gym.car.env_jax import Car2D as Car
 from target_gym.plane.env import EnvParams as PlaneParams
 from target_gym.plane.env_gymnasium import Airplane2D as PlaneGymnasium
 from target_gym.plane.env_jax import Airplane2D as Plane
+
+try:
+    __version__ = version("target-gym")
+except PackageNotFoundError:
+    __version__ = "0.0.0"  # fallback for dev environments
 
 __all__ = (
     "Car",
